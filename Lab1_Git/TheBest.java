920c3aa386cae829a5928c23329ca82f5a3cbd34
FelixMeunier <fmeun013@uottawa.ca>
2019-09-17 18:46:02 -0400

    TheBest was coded

--- Lab1_Git/TheBest.java
+++ Lab1_Git/TheBest.java
@@ -1,7 +1,11 @@
 
 public class TheBest {
-    
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+		PersonInfo1.displayFr();
+		PersonInfo2.displayFr();
+		PersonInfo3.displayFr();
+		PersonInfo4.displayFr();
+		PersonInfo5.displayFr();
     }
 }
