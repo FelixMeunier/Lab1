c712ee3feb65cc6b8c2b8042295e3cd9126ec574
FelixMeunier <fmeun013@uottawa.ca>
2019-09-17 18:38:52 -0400

    Finish merge

--- Lab1_Git/PersonInfo2.java
+++ Lab1_Git/PersonInfo2.java
@@ -2,8 +2,13 @@ public class PersonInfo2 {
 
     public static String firstName = "RJ";
     public static String lastName = "Stead";
+<<<<<<< HEAD
     public static int studentNo = 300072868;
     public static String description = "MacBook";
+=======
+    public static int studentNo = 35534;
+    public static String description = "Je marque n'importe quoi.";
+>>>>>>> ee96bc2e7bedc299d60501cf3ffd1fe7b9b67613
 
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
