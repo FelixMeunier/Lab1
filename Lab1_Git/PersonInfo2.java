91ba2631fa17cbd420251272c1d4f5e67121f9fa
RJStead <rstea073@uottawa.ca>
2019-09-17 18:33:52 -0400

    Mise à jour de la description.

--- Lab1_Git/PersonInfo2.java
+++ Lab1_Git/PersonInfo2.java
@@ -3,7 +3,7 @@ public class PersonInfo2 {
     public static String firstName = "RJ";
     public static String lastName = "Stead";
     public static int studentNo = 35534;
-    public static String description = "MacBook";
+    public static String description = "Je marque n'importe quoi.";
 
     public static void displayFr() {
         String out = SIConstants.GREETING_FR;
